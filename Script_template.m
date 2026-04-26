e8e7887b1ed844aba95a125803236bc6ac066390
Zeming-111 <13700023044@163.com>
2026-04-26 20:30:13 +0800

    Add temp_monitor function (Task 2)

--- Script_template.m
+++ Script_template.m
@@ -120,7 +120,7 @@ fprintf(fileID, '%s', outputText);
 fclose(fileID);
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 
-% Insert answers here
+temp_monitor(a);
 
 
 %% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [30 MARKS]
