b6af64f7b3a7a00ef8c7dc303139dc2bd8cd7bd6
Zeming-111 <13700023044@163.com>
2026-04-26 13:13:27 +0800

    Try to communicate with aiduino which can let led on each 0.5s and then off at the end

--- Script_template.m
+++ Script_template.m
@@ -5,7 +5,25 @@
 
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
 
-% Insert answers here
+% c) Arduino Communication
+% My Arduino port is COM 8
+clear a
+a = arduino("COM8","Uno");
+
+% Define LED digital pin
+ledPin = "D8";
+
+% Blink the LED at 0.5 s intervals
+for i = 1:10
+    writeDigitalPin(a, ledPin, 1);   % ON
+    pause(0.5);
+
+    writeDigitalPin(a, ledPin, 0);   % OFF
+    pause(0.5);
+end
+
+% LED is off at the end
+writeDigitalPin(a, ledPin, 0);
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
